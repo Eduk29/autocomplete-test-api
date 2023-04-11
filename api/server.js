ee826ee32670676286713473b6e6b55c5e5cb38f
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2023-04-10 22:41:07 -0300

    feat: updated cors configuration

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(
 );
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "https://autocomplete-test.vercel.app/");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 });
